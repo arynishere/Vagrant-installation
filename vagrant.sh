69195909ac1aac028c0fc0b56dc8272bce4cf69e
Aryn <95162220+arynishere@users.noreply.github.com>
2024-10-27 10:55:47 +0330

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -39,8 +39,6 @@ echo " install vritual box ..."
 sleep 3
 
 
-#!/bin/bash
-
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
